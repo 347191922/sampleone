da6a5c60df4695688067f8a89fe3e6a4f6f41c20
vagrant <vagrant@homestead>
2018-11-09 08:07:44 +0000

    Move user model to models foldr

---
+++ app/Models/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     * fillable 在过滤用户提交的字段，只有包含在该属性中的字段才能够被正常更新
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     * 当我们需要对用户密码或其它敏感信息在用户实例通过数组或 JSON 显示时进行隐藏
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    /**
+     * Eloquent 模型可以让我们很方便的与数据库进行交互，
+     * 因此我们需要在 Eloquent 模型中借助对 table 属性的定义，
+     * 来指明要进行数据库交互的数据库表名称，在用户模型中，
+     * 我们对应要交互的数据库表为 users
+     */
+    protected $table = 'users';
+}
